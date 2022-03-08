9aa698d04738e97bea0fcd8ce391f21a8c407ff2
dheerajbhagat15 <dheerajbhagat0@gmail.com>
2022-03-09 01:55:43 +0530

    assingment completed.

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,24 +1,21 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema( {
+const userModel = new mongoose.Schema( {
     firstName: String,
-    lastName: String,
-    mobile: {
-        type: String,
+    balance: {type : Number, default : 100}, 
+    address: String,
+    age : Number,
+    gender : { type : String,
+        enum : [ "male", "female", "other"]},
+    isFreeAppUser : {
+        type : Boolean,
+        default : false
+    }    
+        
 
-        required: true
-    },
-    emailId: String,
-    gender: {
-        type: String,
-        enum: ["male", "female", "LGBTQ"] //"falana" will give an error
-    },
-    age: Number,
 }, { timestamps: true });
 
-module.exports = mongoose.model('User', userSchema) //users
+module.exports = mongoose.model('userModel', userModel) 
 
 
 
-// String, Number
-// Boolean, Object/json, array
